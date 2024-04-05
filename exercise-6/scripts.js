87276b67ccb86253931e3ed738632585ea8aee05
Emily Sun <sune086@newschool.edu>
2024-04-04 22:03:00 -0400

    exercise6

---
+++ exercise-6/scripts.js
@@ -0,0 +1,48 @@
+// 按照颜色将方块分组
+let A =document.querySelectorAll(".A")
+let B = document.querySelectorAll(".B")
+let C = document.querySelectorAll(".C")
+let D = document.querySelectorAll(".D")
+let E = document.querySelectorAll(".E")
+// 每组固定颜色
+let colorA=["#ffec80","#83c847","#19aa77","#3d5cc2","#d3c9f7"]
+let colorB=["#fff2aa","#9dd56c","#46c598","#627cce","#8f77d6"]
+let colorC=["#bce49a","#6cd5b0","#6c94d5","#a08cde","#ffc14f"]
+let colorD=["#a2e4cd","#91bbec","#b9abee","#ffcd4f","#2c9c16"]
+let colorE=["#adccf0","#c5b9f2","#ffe067","#63ab24","#009174"]
+// 随机颜色位于array位置
+let randomA=Math.floor(Math.random()*colorA.length)
+let randomB=Math.floor(Math.random()*colorB.length)
+let randomC=Math.floor(Math.random()*colorC.length)
+let randomD=Math.floor(Math.random()*colorD.length)
+let randomE=Math.floor(Math.random()*colorE.length)
+// 随机颜色
+let randomColorA=colorA[randomA]
+let randomColorB=colorB[randomB]
+let randomColorC=colorC[randomC]
+let randomColorD=colorD[randomD]
+let randomColorE=colorE[randomE]
+// 着色
+A[0].style.backgroundColor=randomColorA
+B[0].style.backgroundColor=randomColorB
+B[1].style.backgroundColor=randomColorB
+B[2].style.backgroundColor=randomColorB
+B[3].style.backgroundColor=randomColorB
+C[0].style.backgroundColor=randomColorC
+D[0].style.backgroundColor=randomColorD
+D[1].style.backgroundColor=randomColorD
+D[2].style.backgroundColor=randomColorD
+D[3].style.backgroundColor=randomColorD
+E[0].style.backgroundColor=randomColorE
+
+function refreshPage() {
+    window.location.reload(); // 重新加载页面
+}
+
+// 每 5 秒自动刷新页面
+setTimeout(refreshPage, 2000);
+
+console.log(A)
+console.log(B)
+console.log(randomA)
+console.log(randomColorA)
